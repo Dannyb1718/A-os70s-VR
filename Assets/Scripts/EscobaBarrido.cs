294f46ee4300f3bb4be4db6cb4b97ec0d93306ab
Ayla <danniebeltran14@gmail.com>
2026-03-19 11:03:26 -0500

    Objetos y Texturas

--- Assets/Scripts/EscobaBarrido.cs
+++ Assets/Scripts/EscobaBarrido.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class EscobaBarrido : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float velocidad = 200f;
+    public float anguloMax = 45f;
 
-    // Update is called once per frame
+    private bool barriendo = false;
+    private float anguloActual = 0f;
+    private int direccion = 1;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            barriendo = true;
+        }
+
+        if (barriendo)
+        {
+            float movimiento = velocidad * Time.deltaTime * direccion;
+            transform.Rotate(Vector3.forward * movimiento);
+            anguloActual += movimiento;
+
+            if (Mathf.Abs(anguloActual) >= anguloMax)
+            {
+                direccion *= -1;
+                anguloActual = 0f;
+                barriendo = false;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Radiola.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class RadiolaController : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource musicaSource;   // Música principal
+    public AudioSource sfxSource;      // Sonido de clic
+    public AudioClip clickSound;       // Sonido al encender
+
+    private bool encendida = false;
+
+    void Start()
+    {
+        Debug.Log("Radiola lista");
+
+        if (musicaSource != null)
+            musicaSource.Stop();
+
+        if (sfxSource != null)
+            sfxSource.Stop();
+    }
+
+    // 👉 CLICK CON MOUSE (para pruebas en PC)
+    void OnMouseDown()
+    {
+        ToggleRadio();
+    }
+
+    // 👉 ACTIVACIÓN POR TRIGGER (VR o colisiones)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ToggleRadio();
+        }
+    }
+
+    // 👉 FUNCIÓN PRINCIPAL
+    public void ToggleRadio()
+    {
+        encendida = !encendida;
+
+        if (encendida)
+        {
+            Debug.Log("RADIOLA ENCENDIDA");
+
+            if (sfxSource != null && clickSound != null)
+                sfxSource.PlayOneShot(clickSound);
+
+            if (musicaSource != null)
+                musicaSource.Play();
+        }
+        else
+        {
+            Debug.Log("RADIOLA APAGADA");
+
+            if (musicaSource != null)
+                musicaSource.Stop();
+        }
+    }
+}
